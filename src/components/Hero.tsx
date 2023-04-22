3e0876231ec802552fd2b6fdc54968a7bc5659b0
bss-johir <bss.johir@gmail.com>
2023-04-22 15:00:19 +0600

    Hero section doent

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,31 @@
+import { logo } from '../assets'
 
 function Hero() {
 
   return (
-    <>
-      <h1>Hero</h1>
-    </>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav
+        className="flex justify-between items-center w-full mb-10 pt-3"
+      >
+        <img src={logo} alt="ai_summarizer_logo" className="w-28" />
+
+        <button
+          type='button'
+          className='black_btn'
+          onClick={() => window.open('https://github.com/j-rayhan')}
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' /> <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className='desc'>
+        Simplify you reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
   )
 }
 
